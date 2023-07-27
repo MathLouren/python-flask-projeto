7773139f95cac2eb78fe498a9f59b89b063f0ddc
MathLouren <83808796+MathLouren@users.noreply.github.com>
2023-07-27 10:51:29 +0000

    Adicionando Flask no projeto

--- replit.nix
+++ replit.nix
@@ -1,6 +1,10 @@
 { pkgs }: {
     deps = [
+        pkgs.python39Packages.pip
+        pkgs.sudo
+        pkgs.python39Packages.pip
         pkgs.qtile
         pkgs.cowsay
+        pkgs.python39Packages.flask
     ];
 }
